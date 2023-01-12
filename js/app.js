8dfbda0033cf2dc0e72d7ae19f922e8361152f81
MuadDib3 <myaddib3@gmail.com>
2023-01-12 20:38:59 +0300

    12.01.23

--- js/app.js
+++ js/app.js
@@ -13,3 +13,23 @@ function hideBurger () {
    burger.classList.remove('nav-burger_active');
    burger.classList.add('nav-burger_closed');
 }
+
+function openCard () {
+   const cardWrapper = document.getElementById('card-wrapper');
+   cardWrapper.classList.add('wrapper-product_opened');
+   document.documentElement.classList.add("no-scroll");
+   // document.body.classList.add("no-scroll");
+}
+
+function closeCard () {
+   const cardWrapper = document.getElementById('card-wrapper');
+   cardWrapper.classList.remove('wrapper-product_opened');
+   document.documentElement.classList.remove("no-scroll");
+   // document.body.classList.add("no-scroll");
+}
+
+const openProductCard = document.querySelectorAll ('.open-product-card');
+for (let i = 0; i < openProductCard.length; i ++) {
+   thisProductCard = openProductCard[i];
+   thisProductCard.addEventListener("click", openCard);
+}
